4565f755eb0cb421a581df38f3d6ec038318b58e
Danieldvir04 <111674069+Danieldvir04@users.noreply.github.com>
2022-11-24 15:10:01 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,12 @@
+/*select * from  Products */
+
+/*select productName,UnitPrice from Products */
+
+/*select productName,UnitPrice from Products where unitprice>50  */
+
+/* select productName,UnitPrice from Products where unitprice<50 and  unitprice>40 */
+
+ /* select productName,UnitPrice,CategoryID from Products where unitprice>50 and (CategoryID= 1 or CategoryID= 2) */
+
+/* select * from Suppliers  */
+
